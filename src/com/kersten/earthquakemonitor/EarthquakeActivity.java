375fb52cafb299caffab62392d7c333f7c9f5c01
Eric Kersten <ekersten@gmail.com>
2013-05-13 17:55:06 -0300

    working preferences

--- src/com/kersten/earthquakemonitor/EarthquakeActivity.java
+++ src/com/kersten/earthquakemonitor/EarthquakeActivity.java
@@ -1,10 +1,17 @@
 package com.kersten.earthquakemonitor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class EarthquakeActivity extends FragmentActivity {
+	
+	private static final int MENU_PREFERENCES = Menu.FIRST + 1;
+	private static final int MENU_UPDATE = Menu.FIRST + 2;
+	private static final int SHOW_PREFERENCES = 1;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +21,25 @@ public class EarthquakeActivity extends FragmentActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_earthquake, menu);
+		super.onCreateOptionsMenu(menu);
+		
+		menu.add(0, MENU_PREFERENCES, Menu.NONE, R.string.menu_preferences);
+		
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		super.onOptionsItemSelected(item);
+		
+		switch (item.getItemId()) {
+			case MENU_PREFERENCES:
+				Intent i = new Intent(this, PreferencesActivity.class);
+				startActivityForResult(i, SHOW_PREFERENCES);
+				return true;
+		}
+		
+		return false;
 	}
 
 }
